43ae063b3ba04f186208d09899db00e7cd31c70c
Ricardo <ricardosandoval344@gmail.com>
2026-07-03 19:49:02 -0600

    Creating the basis of the project

---
+++ backend/infrastructure/database.go
@@ -0,0 +1,25 @@
+package infrastructure
+
+import (
+	"time"
+
+	"github.com/RicardoSandoval11/apartamentos/backend/constants"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func GetDatabase() *gorm.DB {
+	db, err := gorm.Open(postgres.Open(constants.DATABASE_CONN_STRING), &gorm.Config{})
+
+	if err != nil {
+		panic("Could not connect to the database")
+	}
+
+	client, _ := db.DB()
+
+	client.SetMaxIdleConns(5)
+	client.SetMaxOpenConns(10)
+	client.SetConnMaxLifetime(time.Hour)
+
+	return db
+}
---
+++ backend/main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/RicardoSandoval11/apartamentos/backend/constants"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.37.0"
+)
+
+/*
+	1. Cada microservicio debe tener un contexto a nivel de servicio que se pasa a los handlers por si se finaliza el pod
+	2. Configuraciones de logging
+	3. Configuracion de OpenTelemetry
+	4. Configuracion de metricas
+	5. Configuracion de los servicios
+	6. Configuracion de los handlers
+	7. Configuracion de una señal si algun agente externo termina el proceso
+	8. Configuracion de circuit breaker usando hystrix
+*/
+
+func main() {
+
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	defer cancel()
+
+	// 2. Configuraciones de logging
+	logger := slog.New(
+		slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		}),
+	).With("service", constants.SERVICE_NAME)
+
+	slog.SetDefault(logger)
+
+	// 3. Configuracion de OpenTelemetry
+	exporter, _ := otlptracegrpc.New(ctx, otlptracegrpc.WithInsecure())
+	metricExporter, _ := otlpmetricgrpc.New(ctx)
+	res, _ := resource.New(
+		ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(constants.SERVICE_NAME),
+			semconv.ServiceVersion(constants.SERVICE_VERSION),
+			attribute.String("environment", constants.ENVIRONMENT),
+		),
+	)
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(exporter),
+		trace.WithResource(res),
+	)
+
+	metricProvider := metric.NewMeterProvider(
+		metric.WithReader(
+			metric.NewPeriodicReader(metricExporter),
+		),
+		metric.WithResource(res),
+	)
+
+	otel.SetTracerProvider(traceProvider)
+	otel.SetMeterProvider(metricProvider)
+
+	otel.SetTextMapPropagator(
+		propagation.NewCompositeTextMapPropagator(
+			propagation.TraceContext{},
+			propagation.Baggage{},
+		),
+	)
+
+	handler := otelhttp.NewHandler(nil, "api-server")
+
+	errs := make(chan error)
+
+	srv := &http.Server{
+		Addr:    constants.APPLICATION_PORT,
+		Handler: handler,
+	}
+
+	go func() {
+		errs <- srv.ListenAndServe()
+	}()
+
+	select {
+	case e := <-errs:
+		{
+			slog.Error(
+				"Application was not able to start",
+				"error", e,
+			)
+		}
+	case <-ctx.Done():
+		{
+			slog.Info("Shutting down")
+
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+
+			_ = traceProvider.Shutdown(shutdownCtx)
+			_ = metricProvider.Shutdown(shutdownCtx)
+			if err := srv.Shutdown(shutdownCtx); err != nil {
+				slog.Error("Shutdown failed", "error", err)
+			}
+		}
+	}
+}
